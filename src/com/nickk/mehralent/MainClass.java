9796c97e3b217598015273cc6663e9430bfb64a9
black-dragon74 <nickk.2974@gmail.com>
2015-12-01 07:07:47 +0530

    Commited all the changes
    Signed-off-by: black-dragon74 <nickk.2974@gmail.com>

---
+++ src/com/nickk/mehralent/MainClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nickk.mehralent;
+
+/**
+ *
+ * @author nick
+ */
+class MainClass {
+    // Change MySQL Config here. Don't mess else keep crying i won't listen.
+    public String url = "jdbc:mysql://localhost:3306/mehrals";
+    public String user = "root";
+    public String pwd = "ExDb20Tra";
+    // Stop. Don't dare to edit lines below. Else you will break your neck.
+    public String cl = "java.sql.DriverManager";
+}
